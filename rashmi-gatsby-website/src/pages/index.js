026596d7d87973b651a6fe04fa58a529d9cedfdf
rashmipbhat <rashmipbhat97@gmail.com>
2021-05-17 23:12:41 +0530

    Initial step1 with some text changes

--- rashmi-gatsby-website/src/pages/index.js
+++ rashmi-gatsby-website/src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+// import { Link } from "gatsby"
+// import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -9,18 +9,9 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people!! I'm Rashmi</h1>
-    <p>Welcome to my new Gatsby site.</p>
-    <StaticImage
-      src=""
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      Welcome to my new Gatsby site. I am a software developer from Bangalore,
+      India.
     </p>
   </Layout>
 )
--- rashmi-gatsby-website/src/pages/page-2.js
+++ rashmi-gatsby-website/src/pages/page-2.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -7,9 +7,7 @@ import Seo from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Images</h1>
   </Layout>
 )
 
