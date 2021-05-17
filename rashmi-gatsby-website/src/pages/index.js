9b23ea61f7e371f55e2e3c1041b489ab1b357570
rashmipbhat <rashmipbhat97@gmail.com>
2021-05-17 22:18:55 +0530

    Initial commit

---
+++ rashmi-gatsby-website/src/pages/index.js
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Hi people!! I'm Rashmi</h1>
+    <p>Welcome to my new Gatsby site.</p>
+    <StaticImage
+      src=""
+      width={300}
+      quality={95}
+      formats={["AUTO", "WEBP", "AVIF"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
